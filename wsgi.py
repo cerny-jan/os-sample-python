cffa5e4e8de3d80d987a4fb95785e5e47099b206
Jan Cerny <jan.cerny@telegraph.co.uk>
2017-05-17 13:46:42 +0100

    hook test

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hellooooo World!"
+    return "Hellooooo World!!!"
 
 if __name__ == "__main__":
     application.run()
